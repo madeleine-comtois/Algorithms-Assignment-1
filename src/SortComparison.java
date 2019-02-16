940ff923ebce549a4f037a3f6ebb01376db027a2
Maddie <maddie.comtois@gmail.com>
2019-02-16 15:44:14 +0000

    added insertionSort method

--- src/SortComparison.java
+++ src/SortComparison.java
@@ -19,7 +19,17 @@
      */
     static double [] insertionSort (double a[]){
 
-        //todo: implement the sort
+        double temp;
+        for (int i = 1; i < a.length; i++) {
+        	for (int j = 1; j > 0; j--) {
+        		if (a[j] < a[j-1]) {
+        			temp = a[j];
+        			a[j] = a[j-1];
+        			a[j-1] = temp;
+        		}
+        	}
+        }
+        return a;
     }//end insertionsort
 
     /**
